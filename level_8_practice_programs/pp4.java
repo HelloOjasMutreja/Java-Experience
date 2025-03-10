ff41eb1bdf4535da528fddf2f788f119570a57ee
HelloOjasMutreja <helloojasmutreja@gmail.com>
2025-03-10 13:28:13 +0530

    lvl8-pp4

---
+++ level_8_practice_programs/pp4.java
@@ -0,0 +1,39 @@
+package level_8_practice_programs;
+
+import java.util.Scanner;
+
+class pp4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[5];
+
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Enter the number " + (i + 1) + " : ");
+            arr[i] = sc.nextInt();
+        }
+
+        for (int x = 0; x < 5; x++) {
+            if (arr[x] > 0) {
+                if (arr[x] % 2 == 0) {
+                    System.out.println("The number " + arr[x] + " is positive and is even.");
+                } else {
+                    System.out.println("The number " + arr[x] + " is positive and is odd.");
+                }
+            } else if (arr[x] == 0) {
+                System.out.println("The number " + arr[x] + " is zero.");
+            } else {
+                System.out.println("The number " + arr[x] + " is negative.");
+            }
+        }
+
+        if (arr[0] > arr[4]) {
+            System.out.println("The first number " + arr[0] + " is greater than the last number " + arr[4]);
+        } else if (arr[0] == arr[4]) {
+            System.out.println("The first number and the last number is equal to " + arr[4]);
+        } else {
+            System.out.println("The first number " + arr[0] + " is lesser than the last number " + arr[4]);
+        }
+
+        sc.close();
+    }
+}
