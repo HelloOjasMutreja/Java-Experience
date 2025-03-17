4baa03f695f4914e908192c13075c53d899ef6c4
HelloOjasMutreja <helloojasmutreja@gmail.com>
2025-03-17 11:24:19 +0530

    lvl9-pp2

---
+++ level_9_practice_programs/pp2.java
@@ -0,0 +1,22 @@
+package level_9_practice_programs;
+
+import java.util.Scanner;
+
+class HandshakeCalc {
+    public static int calculator(int n) {
+        int h = (n * (n - 1) / 2);
+        return h;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of people: ");
+        int x = sc.nextInt();
+        sc.close();
+
+        int max = calculator(x);
+        int min = 0;
+        System.out.println("The maximum number of handshakes is: " + max + " and the min is: " + min + " among " + x
+                + " students");
+    }
+}
